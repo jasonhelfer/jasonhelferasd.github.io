dcad5a7243a1ac6eb033ec4557d08da592c57c40
jasonhelfer <111073124+jasonhelfer@users.noreply.github.com>
2024-11-08 15:52:09 +0000

    snake

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -195,7 +195,23 @@ function handleAppleCollision() {
  
   var row = 0;
   var column = 0;
-
+  console.log(snake.tail.direction)
+  if (snake.tail.direction === "left"){
+    row = snake.tail.row
+    column = snake.tail.column + 1
+   } 
+   if (snake.tail.direction === "right"){
+    row = snake.tail.row
+    column = snake.tail.column - 1
+   }
+   if (snake.tail.direction === "up"){
+    row = snake.tail.row - 1
+    column = snake.tail.column
+   }
+   if (snake.tail.direction === "down"){
+    row = snake.tail.row + 1
+    column = snake.tail.column
+   }
   // code to determine the row and column of the snakeSquare to add to the snake
 if(snake.tail.direction == RIGHT) {
   snake.tail.row ++;
